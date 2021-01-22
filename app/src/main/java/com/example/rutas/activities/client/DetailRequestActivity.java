63b77719cf51cd168fb82cefedc549be942c9551
fimercadog <fimercadog@gamil.com>
2021-01-22 13:06:47 -0500

    primer commit

---
+++ app/src/main/java/com/example/rutas/activities/client/DetailRequestActivity.java
@@ -0,0 +1,152 @@
+package com.example.rutas.activities.client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.JointType;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.SquareCap;
+import com.example.rutas.R;
+import com.example.rutas.includes.MyToolbar;
+import com.example.rutas.providers.GoogleApiProvider;
+import com.example.rutas.utils.DecodePoints;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class DetailRequestActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
+
+    private double mExtraOriginLat;
+    private double mExtraOriginLng;
+    private double mExtraDestinationLat;
+    private double mExtraDestinationLng;
+    private String mExtraOrigin;
+    private String mExtraDestination;
+
+    private LatLng mOriginLatLng;
+    private LatLng mDestinationLatLng;
+
+    private GoogleApiProvider mGoogleApiProvider;
+
+    private List<LatLng> mPolylineList;
+    private PolylineOptions mPolylineOptions;
+
+    TextView mTextViewOrigin;
+    TextView mTextViewDistination;
+    TextView mTextViewTime;
+    TextView mTextViewDistance;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_request);
+        MyToolbar.show(this, "TUS DATOS", true);
+
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
+
+        mExtraOriginLat = getIntent().getDoubleExtra("origin_lat", 0);
+        mExtraOriginLng = getIntent().getDoubleExtra("origin_lng", 0);
+        mExtraDestinationLat = getIntent().getDoubleExtra("destination_lat", 0);
+        mExtraDestinationLng = getIntent().getDoubleExtra("destination_lng", 0);
+        mExtraOrigin = getIntent().getStringExtra("origin");
+        mExtraDestination = getIntent().getStringExtra("destination");
+
+
+        mOriginLatLng = new LatLng(mExtraOriginLat, mExtraOriginLng);
+        mDestinationLatLng = new LatLng(mExtraDestinationLat, mExtraDestinationLng);
+
+        mGoogleApiProvider = new GoogleApiProvider(DetailRequestActivity.this);
+
+        mTextViewOrigin = findViewById(R.id.textViewOrigin);
+        mTextViewDistination = findViewById(R.id.textViewDestination);
+        mTextViewTime = findViewById(R.id.textViewTime);
+        mTextViewDistance = findViewById(R.id.textViewDistance);
+
+        mTextViewOrigin.setText(mExtraOrigin);
+        mTextViewDistination.setText(mExtraDestination);
+
+    }
+
+    private void drawRoute() {
+        mGoogleApiProvider.getDirections(mOriginLatLng, mDestinationLatLng).enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                try {
+
+                    JSONObject jsonObject = new JSONObject(response.body());
+                    JSONArray jsonArray = jsonObject.getJSONArray("routes");
+                    JSONObject route = jsonArray.getJSONObject(0);
+                    JSONObject polylines = route.getJSONObject("overview_polyline");
+                    String points = polylines.getString("points");
+                    mPolylineList = DecodePoints.decodePoly(points);
+                    mPolylineOptions = new PolylineOptions();
+                    mPolylineOptions.color(Color.DKGRAY);
+                    mPolylineOptions.width(13f);
+                    mPolylineOptions.startCap(new SquareCap());
+                    mPolylineOptions.jointType(JointType.ROUND);
+                    mPolylineOptions.addAll(mPolylineList);
+                    mMap.addPolyline(mPolylineOptions);
+
+                    JSONArray legs = route.getJSONArray("legs");
+                    JSONObject leg = legs.getJSONObject(0);
+                    JSONObject distance = leg.getJSONObject("distance");
+                    JSONObject duration = leg.getJSONObject("duration");
+                    String distanceText = distance.getString("text");
+                    String durationText = duration.getString("text");
+                    mTextViewTime.setText(durationText);
+                    mTextViewDistance.setText(distanceText);
+
+
+                } catch (Exception e) {
+                    Log.d("Error", "Error encontrado " + e.getMessage());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        mMap.addMarker(new MarkerOptions().position(mOriginLatLng).title("Origen").icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_pin_red)));
+        mMap.addMarker(new MarkerOptions().position(mDestinationLatLng).title("Destino").icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_pin_blue)));
+
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(
+                new CameraPosition.Builder()
+                        .target(mOriginLatLng)
+                        .zoom(14f)
+                        .build()
+        ));
+
+        drawRoute();
+    }
+}
---
+++ app/src/main/java/com/example/rutas/providers/DriverProvider.java
@@ -0,0 +1,20 @@
+package com.example.rutas.providers;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.example.rutas.models.Driver;
+
+public class DriverProvider {
+
+    DatabaseReference mDatabase;
+
+    public DriverProvider() {
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child("Drivers");
+    }
+
+    public Task<Void> create(Driver driver) {
+        return mDatabase.child(driver.getId()).setValue(driver);
+    }
+
+}
